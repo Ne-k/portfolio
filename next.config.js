bc0bcd3a4cf6e5af77b04c6f8f09eddbd357716f
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-07-21 10:29:15 -0700

    Added a note

--- next.config.js
+++ next.config.js
@@ -11,6 +11,25 @@ module.exports = {
     ]
   },
 
+  /*
+  rewrites: async () => {
+    // In order to support wildcard subdomains with different content, use a "rewrite" to include the host in the path
+    return [
+      {
+        source: '/:path*{/}?',
+        has: [
+          {
+            type: 'host',
+            value: '(?<siteHost>.*)',
+          },
+        ],
+        destination: '/site/:siteHost/:path*',
+      },
+    ];
+  },
+
+   */
+
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
