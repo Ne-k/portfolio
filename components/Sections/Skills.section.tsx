2845f89f113f060390e1ddc0fd6a47e82b65c443
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-07-20 20:14:36 -0700

    yoink

--- components/Sections/Skills.section.tsx
+++ components/Sections/Skills.section.tsx
@@ -3,7 +3,7 @@ import { BsArrowRightShort } from "../Misc/Icons.collection";
 const Skills = () => {
   return (
     <div className="my-16 px-3 font-sen text-white" id="skills">
-      <p className="text-3xl font-bold text-white">Skills & Uses</p>
+      <p className="text-3xl font-bold text-white">Skills</p>
 
       <div className="text-md my-8 flex flex-col font-medium md:text-xl custom:text-lg">
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
