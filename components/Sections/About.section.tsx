95095fb0e043bf76ca0b7193ed140ccf98c8478f
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-07-21 10:22:51 -0700

    added viewport meta tag

--- components/Sections/About.section.tsx
+++ components/Sections/About.section.tsx
@@ -7,40 +7,42 @@ import { HiOutlineArrowNarrowRight } from "../Misc/Icons.collection";
 
 const About: NextComponentType = () => {
   return (
-    <div className="my-8 flex flex-row items-center justify-between px-3 font-sen">
-      <div>
-        <p className="text-3xl font-bold text-white">Cardin Nguyen</p>
-        <p className="mt-1 text-lg text-gray-300">
-          Backend developer and a student
-        </p>
+      <>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <div className="my-8 flex flex-row items-center justify-between px-3 font-sen">
+              <div>
+                  <p className="text-3xl font-bold text-white">Cardin Nguyen</p>
+                  <p className="mt-1 text-lg text-gray-300">
+                      Backend developer and a student
+                  </p>
 
-        <p className="mt-4 text-gray-400">
-          I&apos;m a backend developer who somehow finds <br/> a way to mess stuff up.
-        </p>
+                  <p className="mt-4 text-gray-400">
+                      I&apos; m a backend developer who somehow finds <br/> a way to mess stuff up.
+                  </p>
 
-          <Link href="/" passHref>
-              <a
-                  className="mt-4 flex cursor-pointer flex-row items-center gap-1 font-jost text-xl text-gray-400 duration-100 hover:ml-2"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                  Read my Resume (Coming Soon)
-                  <HiOutlineArrowNarrowRight />
-              </a>
-          </Link>
+                  <Link href="/" passHref>
+                      <a
+                          className="mt-4 flex cursor-pointer flex-row items-center gap-1 font-jost text-xl text-gray-400 duration-100 hover:ml-2"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >
+                          Read my Resume (Coming Soon)
+                          <HiOutlineArrowNarrowRight/>
+                      </a>
+                  </Link>
 
-      </div>
+              </div>
 
-      <div className="hidden custom:block">
-        <Image
-          src="/assests/avatar.png"
-          width="112"
-          height="112"
-          className="rounded-full"
-          alt="avatar"
-        />
-      </div>
-    </div>
+              <div className="hidden custom:block">
+                  <Image
+                      src="/assests/avatar.png"
+                      width="112"
+                      height="112"
+                      className="rounded-full"
+                      alt="avatar"/>
+              </div>
+          </div>
+      </>
   );
 };
 
