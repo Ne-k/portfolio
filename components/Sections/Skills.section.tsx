869d4edaf223aa7e8ac5a9fde87074f2c0f426a5
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-07-20 20:06:03 -0700

    Revert "yoink"
    This reverts commit ebddf019166a0176e8e8d9773151691e65b5a12b.

--- components/Sections/Skills.section.tsx
+++ components/Sections/Skills.section.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
           <span className="text-white">Photography</span>
-          &nbsp;I take photos with a Nikon D7500 DLSR camera
+          &nbsp;I take photos with a Nikon D7500 DLSR cameras
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
