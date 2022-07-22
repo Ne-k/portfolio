f4283cebf0a758f84866c34ae21d78eca19e7d3a
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-07-22 10:39:03 -0700

    asda

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -15,8 +15,7 @@ const getAccessToken = async () => {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     body: new URLSearchParams({
-      grant_type: 'refresh_token',
-      refresh_token
+      grant_type: 'client_credentials'
     })
   });
 
