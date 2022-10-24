c64fa9de10e899c594fa22a9eba2497c429b85b7
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-10-24 09:19:41 -0700

    asd

--- components/Misc/Icons.collection.tsx
+++ components/Misc/Icons.collection.tsx
@@ -2,7 +2,9 @@ import { BsSpotify, BsArrowRightShort } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import { AiOutlineTwitter, AiOutlineGithub } from "react-icons/ai";
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
-import { FaDiscord, FaInstagram } from "react-icons/fa";
+import {SiTypescript} from "react-icons/si";
+import {FiCamera} from "react-icons/fi";
+import { FaDiscord, FaInstagram, FaJava, FaPython, FaNodeJs, FaReact, } from "react-icons/fa";
 import { VscGithubAlt } from "react-icons/vsc";
 
 export {
@@ -15,4 +17,10 @@ export {
   FaDiscord,
     FaInstagram,
   VscGithubAlt,
+    FaJava,
+    FaPython,
+    FaNodeJs,
+    FaReact,
+    SiTypescript,
+    FiCamera
 };
--- components/Sections/Skills.section.tsx
+++ components/Sections/Skills.section.tsx
@@ -1,39 +1,40 @@
-import { BsArrowRightShort } from "../Misc/Icons.collection";
+import { BsArrowRightShort, FaJava, FaNodeJs, SiTypescript, FiCamera } from "../Misc/Icons.collection";
 
 const Skills = () => {
   return (
     <div className="my-16 px-3 font-sen text-white" id="skills">
-      <p className="text-3xl font-bold text-white text-center">Skills</p>
+      <p className="text-3xl font-bold text-white">Skills</p>
 
       <div className="text-md my-8 flex flex-col font-medium md:text-xl custom:text-lg">
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
-          <BsArrowRightShort size="30" />
+          <FiCamera size="30" />
           <span className="text-white">Photography</span>
-          &nbsp;I take photos with a Nikon D7500 DSLR camera
+          &nbsp;I take photos with a Nikon D7500 DLSR camera
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
-          <BsArrowRightShort size="30" />
+          <FaNodeJs size="30" />
           <span className="text-white">Javascript </span>
-          &nbsp;the language I use for small projects I want done quickly
+          &nbsp; the language I use for small projects I want done quickly
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
-          <BsArrowRightShort size="30" />
-          <span className="text-white">C++ </span>
-          &nbsp;the language I use for designing windows apps
+          <SiTypescript size="30" />
+          <span className="text-white">Typescript </span>
+          &nbsp; as the language I use for small private projects
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
-          <BsArrowRightShort size="30" />
+          <FaJava size="30" />
           <span className="text-white">Java </span>
-          &nbsp;the language I mainly use for FRC team 7034
+          &nbsp; the language I mainly use for FRC team 7034
         </p>
       </div>
 
-      <p className="text-lg font-medium text-slate-300 text-center">
-        ...more skills include <span className="text-white">ReactJS</span>,{" "}
-        <span className="text-white">NextJS</span>{" "}
+      <p className="text-lg font-medium text-slate-300">
+        ...more skills include <span className="text-white">NextJS</span>,{" "}
+        <span className="text-white">Reactjs</span>,{" "}
+        <span className="text-white">C++ </span>{" "}
       </p>
     </div>
   );
